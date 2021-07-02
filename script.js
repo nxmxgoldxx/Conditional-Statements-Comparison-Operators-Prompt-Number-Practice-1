83cf7d4511bc318dddbe6fdbd88a4db1a751ff33
NXMX GXLDXX <nxmxgoldxx@live.com>
2021-07-01 17:10:54 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var twelveString = "12";
+var twelveNum = 12;
+
+if (twelveString === twelveNum) {
+  console.log("Oh yeah, those two variables types and values are the same.");
+} else if (twelveString == twelveNum) {
+  console.log("The value of both variables is the same.");
+} else {
+  console.log("Something is incorrect, review your formula.");
+}
